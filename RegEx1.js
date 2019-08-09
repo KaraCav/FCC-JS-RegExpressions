91399fbfa12ad9527c81175bdd2ea8eeb12bb905
Kara Cavanaugh <karacav@msn.com>
2019-08-09 16:41:42 -0400

    Number 2

--- RegEx1.js
+++ RegEx1.js
@@ -2,3 +2,8 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex.test(myString);
+
+// 2. Regular Expressions: Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; 
+let result = petRegex.test(petString);
