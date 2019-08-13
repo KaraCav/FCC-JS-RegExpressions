f96b2583107d83ea8dec1671a7d27c865953a1a5
Kara Cavanaugh <karacav@msn.com>
2019-08-13 15:25:08 -0400

    Number 3 and 4

--- RegEx1.js
+++ RegEx1.js
@@ -7,3 +7,13 @@ let result = myRegex.test(myString);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/; 
 let result = petRegex.test(petString);
+
+// 3. Ignore Case While Matching
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; 
+let result = fccRegex.test(myString);
+
+// 4.  Extract Matches- Using the Match Method
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/;
+let result = extractStr.match(codingRegex); 
