7846799a0d975007c98a6302a0357ecd10d0d93e
Kara Cavanaugh <karacav@msn.com>
2019-08-13 15:57:17 -0400

    Numbers 5-7

--- RegEx1.js
+++ RegEx1.js
@@ -17,3 +17,21 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex); 
+
+// 5. Find More Than the First Match Using -g Flag
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; 
+let result = twinkleStar.match(starRegex);
+
+// 6. Match Anything with Wildcard Period
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; 
+let result = unRegex.test(exampleStr);
+
+// 7.  Match Single Character with Multiple Possibilities
+// Character classes allow you to define a group of characters you wish to match by placing them  
+// inside square ([ and ]) brackets. This searches for a literal pattern with flexibility.
+// For example, you want to match "bag", "big", and "bug" but not "bog". You can do /b[aiu]g/ to do this. 
+let quoteSample = "Beware of bugs in the above code.";
+let vowelRegex = /[aeiou]/gi; 
+let result = quoteSample.match(vowelRegex); 
