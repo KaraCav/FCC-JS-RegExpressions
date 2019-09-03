9e861b3351d92a0c497d64a1da07940dc694399e
Kara Cavanaugh <karacav@msn.com>
2019-09-03 17:45:13 -0400

    Number 15 & 16

--- RegEx2.js
+++ RegEx2.js
@@ -4,3 +4,14 @@ let reCriminals = /C+/g;
 let matchedCriminals = crowd.match(reCriminals);
 // without '+', returns "C,C,C"
 // WITH '+', returns "CCC"
+
+// 15.  Match Beginning String Patterns
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // returns true if line begins with 'Cal'
+let result = calRegex.test(rickyAndCal);
+
+// 16. Match Ending String Patterns
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // returns true if last word is 'caboose'
+let result = lastRegex.test(caboose);
+
