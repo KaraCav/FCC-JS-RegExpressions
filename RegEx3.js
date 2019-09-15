ceb9b0a0443a8195344ce6922422b1603f66c930
Kara Cavanaugh <karacav@msn.com>
2019-09-15 14:39:24 -0400

    Through Number 23

--- RegEx3.js
+++ RegEx3.js
@@ -1,7 +1,17 @@
-// 21.) Restrict Possible Usernames: 
+// 21. Restrict Possible Usernames: 
 // a) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
 // b) Username letters can be lowercase and uppercase.
 // c) Usernames have to be at least two characters long. A two-letter username can only use alphabet letter characters.
 let username = "JackOfAllTrades";
-let userCheck = /^[a-z]{2,}d*$/i; // (^ first letters) ($ last letters) (* 0 or more)
+let userCheck = /^[a-z]{2,}\d*$/i; // (^ first letters) ($ last letters) (* 0 or more)
 let result = userCheck.test(username); // this returns 'true'
+
+// 22. Match Whitespace
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g;
+let result = sample.match(countWhiteSpace);
+
+// 23. Match Non-Whitespace Characters
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; 
+let result = sample.match(countNonWhiteSpace);
